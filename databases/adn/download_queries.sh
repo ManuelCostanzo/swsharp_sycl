a5b91b4ebc7cf0194d962a7a2e752e6cdb409227
Manuel Costanzo <manuelcostanzo22@gmail.com>
2023-04-17 16:26:45 -0300

    FIX

--- databases/adn/download_queries.sh
+++ databases/adn/download_queries.sh
@@ -1,2 +1,5 @@
-wget "https://drive.google.com/uc?export=download&id=12zT7ZAfp909CvPXGCXO_HN_U_jaAeqco&confirm=t" -O queries.zip
-unzip queries.zip
+wget --no-check-certificate -O queries.tar.gz 'https://drive.google.com/u/0/uc?id=1wWPZcepSnyavIhGL_eCOVIabJtkMquOq&export=download&confirm=t&uuid=870f4e23-5600-4b23-bbad-fe8e7785aa24&at=ANzk5s6M9eX4j9fE0eiouHAtH8hl:1681759576585'
+
+tar -xzvf queries.tar.gz
+
+
---
+++ databases/protein/download_queries.sh
@@ -0,0 +1,5 @@
+
+wget --no-check-certificate -O queries.tar.gz 'https://drive.google.com/u/0/uc?id=1JorMVhY_jHZmok3Gktjkg9y5CYT7jemQ&export=download&confirm=t&uuid=dc13607b-2d82-4b25-8a15-e048d5f16311&at=ANzk5s630tyH6ohY-hBaXlqE6CER:1681758399201'
+
+tar -xzvf queries.tar.gz
+
