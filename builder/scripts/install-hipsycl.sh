a0bc7eb492357309ff1b066230c5347e9acb3654
Manuel Costanzo <manuelcostanzo22@gmail.com>
2023-08-11 11:49:20 -0300

    New Dockerfile

--- builder/scripts/install-hipsycl.sh
+++ builder/scripts/install-hipsycl.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/opt/hipSYCL/llvm/lib/:$LD_LIBRARY_PATH
 export PATH=/opt/hipSYCL/llvm/bin:$PATH
-export HIPSYCL_INSTALL_PREFIX=${HIPSYCL_INSTALL_PREFIX:-/opt/hipSYCL}
+export HIPSYCL_INSTALL_PREFIX=/opt/hipSYCL
 
 set -e
-HIPSYCL_BUILD_DIR=${HIPSYCL_BUILD_DIR:-/tmp/hipSYCL-installer}
-HIPSYCL_REPO_USER=${HIPSYCL_REPO_USER:-illuhad}
-HIPSYCL_REPO_BRANCH=${HIPSYCL_REPO_BRANCH:-develop}
-HIPSYCL_WITH_CUDA=${HIPSYCL_WITH_CUDA:-ON}
-HIPSYCL_WITH_ROCM=${HIPSYCL_WITH_ROCM:-ON}
-HIPSYCL_LLVM_DIR=${HIPSYCL_LLVM_DIR:-/opt/hipSYCL/llvm/lib/}
+HIPSYCL_BUILD_DIR=/tmp/hipSYCL-installer
+HIPSYCL_LLVM_DIR=/opt/hipSYCL/llvm/lib/
 
-if [ -d "$HIPSYCL_BUILD_DIR" ]; then
-       read -p  "The build directory already exists, do you want to use $HIPSYCL_BUILD_DIR anyways?[y]" -n 1 -r
-       echo 
-       if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-              echo "Please specify a different directory, exiting"
-              [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-       else
-              echo "Using the exisiting directory"
-       fi
-else
+
 echo "Cloning hipSYCL"
-git clone --recurse-submodules -b $HIPSYCL_REPO_BRANCH https://github.com/$HIPSYCL_REPO_USER/hipSYCL $HIPSYCL_BUILD_DIR
-#wget https://github.com/illuhad/hipSYCL/archive/refs/tags/v0.9.3.zip -O /tmp/hipSYCL.zip
-#unzip /tmp/hipSYCL.zip -d /tmp
-#mv /tmp/hipSYCL-0.9.3 $HIPSYCL_BUILD_DIR
-
-
-fi
+git clone --recurse-submodules -b develop https://github.com/illuhad/hipSYCL $HIPSYCL_BUILD_DIR
 
 mkdir -p $HIPSYCL_BUILD_DIR/build
 cd $HIPSYCL_BUILD_DIR/build
@@ -36,9 +17,10 @@ cd $HIPSYCL_BUILD_DIR/build
 cmake \
 -DDWITH_SSCP_COMPILER=ON \
 -DWITH_CPU_BACKEND=ON \
--DWITH_CUDA_BACKEND=$HIPSYCL_WITH_CUDA \
+-DWITH_CUDA_BACKEND=ON \
 -DWITH_ROCM_BACKEND=ON \
 -DLLVM_DIR=$HIPSYCL_LLVM_DIR \
+-DROCM_PATH=/opt/rocm \
 -DCMAKE_INSTALL_PREFIX=$HIPSYCL_INSTALL_PREFIX \
 ..
 
--- builder/scripts/load-intel-llvm.sh
+++ builder/scripts/load-intel-llvm.sh
@@ -3,4 +3,5 @@
 export DPCPP_HOME=/opt/build/intel
 export PATH=$DPCPP_HOME/llvm/build/bin:$PATH
 export LD_LIBRARY_PATH=$DPCPP_HOME/llvm/build/lib:$LD_LIBRARY_PATH
+source /opt/intel/oneapi/setvars.sh
 
