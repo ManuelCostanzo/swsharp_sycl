8c658baadb392a5f3f6819402ec07126d189df36
ManuelCostanzo <manuelcostanzo22@gmail.com>
2023-08-12 16:40:16 -0300

    Update install-intel-llvm.sh

--- builder/scripts/install-intel-llvm.sh
+++ builder/scripts/install-intel-llvm.sh
@@ -3,5 +3,6 @@ mkdir -p /opt/build/intel
 cd /opt/build/intel
 git clone https://github.com/intel/llvm.git -b sycl
 cd /opt/build/intel/llvm
-CUDA_LIB_PATH=/usr/local/cuda/lib64/stubs CC=gcc CXX=g++ python3 ./buildbot/configure.py --cuda --cmake-opt="-DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda" --hip --cmake-opt="-DSYCL_BUILD_PI_HIP_ROCM_DIR=/opt/rocm" -t release
+# CUDA_LIB_PATH=/usr/local/cuda/lib64/stubs CC=gcc CXX=g++ python3 ./buildbot/configure.py --enable-all-llvm-targets --shared-libs --cuda --hip --cmake-opt="-DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda" --cmake-opt="-DSYCL_BUILD_PI_HIP_ROCM_DIR=/opt/rocm" -t release
+CUDA_LIB_PATH=/usr/local/cuda/lib64/stubs CC=gcc CXX=g++ python3 ./buildbot/configure.py --enable-all-llvm-targets --shared-libs --hip --cmake-opt="-DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda" --cmake-opt="-DSYCL_BUILD_PI_HIP_ROCM_DIR=/opt/rocm" -t release
 CUDA_LIB_PATH=/usr/local/cuda/lib64/stubs CC=gcc CXX=g++ python3 ./buildbot/compile.py
